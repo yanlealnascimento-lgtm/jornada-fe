5ed55a9a8dad0330b74ae64897721636d6889542
yanlealnascimento-lgtm <yanleal.nascimento@ucsal.edu.br>
2026-06-13 01:34:25 -0300

    Fix compileSdk for isar_flutter_libs to fix AAPT lStar resource error

--- app/android/build.gradle.kts
+++ app/android/build.gradle.kts
@@ -20,12 +20,15 @@ subprojects {
 }
 
 // Fix: isar_flutter_libs 3.x não define namespace, exigido pelo AGP 8+
+// e usa compileSdkVersion 30, que não conhece atributos do AGP/AndroidX
+// mais recentes (ex: android:attr/lStar), causando falha no AAPT.
 gradle.afterProject {
     if (plugins.hasPlugin("com.android.library")) {
         val android = extensions.findByType(com.android.build.gradle.LibraryExtension::class.java)
         if (android?.namespace == null) {
             android?.namespace = group.toString()
         }
+        android?.compileSdk = 35
     }
 }
 
